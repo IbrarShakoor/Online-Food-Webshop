e4e5b2f63faaf1f83ba6c8a891b9b122cdab1d6f
Ibrar Shakoor <68583803+IbrarShakoor@users.noreply.github.com>
2020-10-02 14:25:31 +0200

    CategoryBean Class(Child class) extends from BaseBean class
    Implementing all of the attributes for restaurants.

---
+++ CategoryBean.java
@@ -0,0 +1,81 @@
+package in.co.online.food.delivery.bean;
+
+
+
+/*Child calss:CategoryBean */
+public class CategoryBean extends BaseBean {
+
+	private long restaurantId;
+	private String restaurantName;
+	private long categoryId;
+	private String categoryName;
+	private String description;
+	private String image;
+	
+	
+	
+	
+	
+	
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public long getRestaurantId() {
+		return restaurantId;
+	}
+
+	public void setRestaurantId(long restaurantId) {
+		this.restaurantId = restaurantId;
+	}
+
+	public String getRestaurantName() {
+		return restaurantName;
+	}
+
+	public void setRestaurantName(String restaurantName) {
+		this.restaurantName = restaurantName;
+	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+
+	public long getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(long categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public String getKey() {
+		// TODO Auto-generated method stub
+		return id+"";
+	}
+
+	@Override
+	public String getValue() {
+		// TODO Auto-generated method stub
+		return categoryName;
+	}
+
+	
+}
